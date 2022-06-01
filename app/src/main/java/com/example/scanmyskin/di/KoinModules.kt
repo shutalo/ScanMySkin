fbf026add1630c0325fb969a78bbd08dc8ebc9df
shutalo <lukasutalo@protonmail.com>
2022-06-01 23:48:41 +0200

    koin updated

--- app/src/main/java/com/example/scanmyskin/di/KoinModules.kt
+++ app/src/main/java/com/example/scanmyskin/di/KoinModules.kt
@@ -1,5 +1,6 @@
 package com.example.scanmyskin.di
 
+import com.example.scanmyskin.BuildConfig
 import com.example.scanmyskin.data.network.NetworkRequests
 import com.example.scanmyskin.data.repository.AuthRepo
 import com.example.scanmyskin.ui.fragments.viewmodels.AuthViewModel
@@ -11,25 +12,35 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val appModules = module {
+val viewModelModules = module{
+    viewModel<AuthViewModel> { AuthViewModel(get()) }
+    viewModel<HomeViewModel> { HomeViewModel() }
 }
 
-val viewModelModules = module{
-    viewModel<AuthViewModel> { AuthViewModel() }
-    viewModel<HomeViewModel> { HomeViewModel() }
-
+val repositoryModule = module {
     single { AuthRepo() }
 }
 
-fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-    return Retrofit.Builder().baseUrl("").client(okHttpClient)
-        .addConverterFactory(GsonConverterFactory.create()).build()
+val networkModule = module {
+    single {
+        Retrofit.Builder()
+            .client(OkHttpClient.Builder().build())
+            .baseUrl(BuildConfig.SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+//    single { get<Retrofit>().create(GithubSearchApi::class.java) }
 }
 
-fun provideOkHttpClient(): OkHttpClient {
-    val logging: HttpLoggingInterceptor = HttpLoggingInterceptor()
-    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-    return OkHttpClient().newBuilder().addInterceptor(logging).build()
-}
-
-fun provideNetwork(retrofit: Retrofit): NetworkRequests = retrofit.create(NetworkRequests::class.java)
+//fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+//    return Retrofit.Builder().baseUrl("").client(okHttpClient)
+//        .addConverterFactory(GsonConverterFactory.create()).build()
+//}
+//
+//fun provideOkHttpClient(): OkHttpClient {
+//    val logging: HttpLoggingInterceptor = HttpLoggingInterceptor()
+//    logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+//    return OkHttpClient().newBuilder().addInterceptor(logging).build()
+//}
+//
+//fun provideNetwork(retrofit: Retrofit): NetworkRequests = retrofit.create(NetworkRequests::class.java)
