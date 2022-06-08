74018b09c3416d075eb87300127c33107a1047ff
shutalo <lukasutalo@protonmail.com>
2022-06-08 17:03:32 +0200

    refactor: shouldShowProgressDialog visibility changed

--- app/src/main/java/com/example/scanmyskin/ui/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/example/scanmyskin/ui/viewmodels/BaseViewModel.kt
@@ -6,10 +6,10 @@ import androidx.lifecycle.ViewModel
 
 open class BaseViewModel : ViewModel() {
 
-    protected var _shouldShowProgressDialog: MutableLiveData<Boolean> = MutableLiveData(false)
+    private var _shouldShowProgressDialog: MutableLiveData<Boolean> = MutableLiveData(false)
     var shouldShowProgressDialog: LiveData<Boolean> = _shouldShowProgressDialog
 
-    protected fun shouldShowProgressDialog(value: Boolean){
+    fun shouldShowProgressDialog(value: Boolean){
         _shouldShowProgressDialog.postValue(value)
     }
 }
