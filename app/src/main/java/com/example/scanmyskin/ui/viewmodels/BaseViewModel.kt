ca73d8d91d52fbff33205ab57dee5e0827ad0b5e
shutalo <lukasutalo@protonmail.com>
2022-06-09 23:02:32 +0200

    refactor: removed unused strings

--- app/src/main/java/com/example/scanmyskin/ui/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/example/scanmyskin/ui/viewmodels/BaseViewModel.kt
@@ -3,10 +3,11 @@ package com.example.scanmyskin.ui.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.scanmyskin.helpers.SingleLiveEvent
 
 open class BaseViewModel : ViewModel() {
 
-    private var _shouldShowProgressDialog: MutableLiveData<Boolean> = MutableLiveData(false)
+    private var _shouldShowProgressDialog: SingleLiveEvent<Boolean> = SingleLiveEvent()
     var shouldShowProgressDialog: LiveData<Boolean> = _shouldShowProgressDialog
 
     fun shouldShowProgressDialog(value: Boolean){
